180dd0dbcd1132cba3ac469248fcede26a965eb9
Daniel Pitfield <danielpitfield1@gmail.com>
2020-09-09 20:23:24 +0100

    Convert to C#

---
+++ McLarenUmbraco/Controllers/EmailController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace McLarenUmbraco.Controllers
+{
+    public class EmailController : SurfaceController
+    {
+        public ActionResult RenderForm()
+        {
+            return PartialView("~/Views/Partials/Forms/Email/Email.cshtml");
+        }
+
+        public ActionResult SubmitForm()
+        {
+            throw new NotImplementedException("Add code here");
+        }
+
+        public ActionResult SendEmail()
+        {
+            throw new NotImplementedException("Add code here");
+        }
+    }
+}
---
+++ McLarenUmbraco/Models/EmailModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace McLarenUmbraco.Models
+{
+    public class EmailModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Recipient { get; set; } // TODO string[] for multiple recipients?
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
