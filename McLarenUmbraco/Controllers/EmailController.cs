6f27c4cfa65cc045d2767f2038340d556de59edd
Daniel Pitfield <danielpitfield1@gmail.com>
2020-09-10 23:17:48 +0100

    Documentation

--- McLarenUmbraco/Controllers/EmailController.cs
+++ McLarenUmbraco/Controllers/EmailController.cs
@@ -1,48 +1,64 @@
-﻿using MailKit.Net.Smtp;
-using McLarenUmbraco.Models;
-using MimeKit;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Mvc;
-using Umbraco.Web.Mvc;
-
-namespace McLarenUmbraco.Controllers
+﻿namespace McLarenUmbraco.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Mvc;
+    using MailKit.Net.Smtp;
+    using McLarenUmbraco.Models;
+    using MimeKit;
+    using Umbraco.Web.Mvc;
+
+    /// <summary>
+    /// Logic for displaying form and sending an email with entered information
+    /// </summary>
     public class EmailController : SurfaceController
     {
+        /// <summary>
+        /// Displays the contact form
+        /// </summary>
+        /// <returns>Partial View of contact form</returns>
         public ActionResult RenderForm()
         {
-            return PartialView("~/Views/Partials/Forms/Email/Email.cshtml");
+            return this.PartialView("~/Views/Partials/Forms/Email/Email.cshtml");
         }
 
+        /// <summary>
+        /// Submit method of the contact form
+        /// </summary>
+        /// <param name="model">Email details for sending</param>
+        /// <returns>The current page (reloads page)</returns>
         public ActionResult SubmitForm(EmailModel model)
         {
-            ViewData["Message"] = null; // Reset confirmation message
-            ViewData["Errors"] = null;
+            this.ViewData["Message"] = null; // Reset confirmation message
+            this.ViewData["Errors"] = null;
 
             if (ModelState.IsValid)
             {
-                SendEmail(model);
-                return CurrentUmbracoPage();
+                this.SendEmail(model);
+                return this.CurrentUmbracoPage();
             }
             else
             {
                 // Determine errors
-                List<string>errorList = new List<string>();
-                foreach (var field in ModelState)
+                List<string> errorList = new List<string>();
+                foreach (var field in this.ModelState)
                 {
                     IEnumerable<string> fieldErrors = field.Value.Errors.Select(error => error.ErrorMessage);
                     errorList.AddRange(fieldErrors);
                 }
 
                 // Send/Pass errors to view
-                ViewData["Errors"] = string.Join(Environment.NewLine, errorList);
+                this.ViewData["Errors"] = string.Join(Environment.NewLine, errorList);
 
-                return CurrentUmbracoPage();
+                return this.CurrentUmbracoPage();
             }
         }
 
+        /// <summary>
+        /// Construct and send email using Gmail SMTP
+        /// </summary>
+        /// <param name="model">Email details for sending</param>
         public void SendEmail(EmailModel model)
         {
             // Prepare Email
@@ -51,11 +67,12 @@ namespace McLarenUmbraco.Controllers
             mailMessage.From.Add(new MailboxAddress(model.Name, "mclaren1umbraco@gmail.com"));
             mailMessage.To.Add(new MailboxAddress("Personal Email", "danielpitfield1@gmail.com"));
 
-            mailMessage.Subject = (string.IsNullOrWhiteSpace(model.Subject)) ? "No subject" : model.Subject;
+            mailMessage.Subject = string.IsNullOrWhiteSpace(model.Subject) ? "No subject" : model.Subject;
 
-            string message = "";
+            string message = string.Empty;
 
-            if (!string.IsNullOrWhiteSpace(model.Email_Address)) // If the email address was specified
+            // If the email address was specified
+            if (!string.IsNullOrWhiteSpace(model.Email_Address))
             {
                 // Begin the message with a paragraph stating the email address (an email to reply to)
                 message = $"<p><b>Reply email address: </b>{model.Email_Address}</p>";
@@ -78,11 +95,12 @@ namespace McLarenUmbraco.Controllers
                 try
                 {
                     smtpClient.Send(mailMessage);
-                    ViewData["Message"] = "Email was sent successfully";
+                    this.ViewData["Message"] = "Email was sent successfully";
                 }
-                catch (Exception ex) // Valid email but it couldn't be sent
+                catch (Exception ex)
                 {
-                    ViewData["Errors"] = "ERROR: Email was not sent";
+                    // Valid email but it couldn't be sent
+                    this.ViewData["Errors"] = $"ERROR: Email was not sent - {ex.Message}";
                 }
 
                 smtpClient.Disconnect(true);
--- McLarenUmbraco/Models/EmailModel.cs
+++ McLarenUmbraco/Models/EmailModel.cs
@@ -1,35 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
-
-namespace McLarenUmbraco.Models
+﻿namespace McLarenUmbraco.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     /// <summary>
-    /// 
+    /// Class describing an email to be sent using the contact form
     /// </summary>
     public class EmailModel
     {
         /// <summary>
-        /// 
+        /// Gets or sets name of the website user wishing to send an email (required field)
         /// </summary>
         [Required]
         public string Name { get; set; }
 
         /// <summary>
-        /// 
+        /// Gets or sets email address given for reply purposes (optional)
         /// </summary>
         [EmailAddress]
         public string Email_Address { get; set; }
 
         /// <summary>
-        /// 
+        /// Gets or sets short summary of the email's content (optional)
         /// </summary>
         public string Subject { get; set; }
 
         /// <summary>
-        /// 
+        /// Gets or sets main body of text for the email (required field)
         /// </summary>
         [Required]
         public string Message { get; set; }
